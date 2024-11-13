80d57e23a3ef013a5db44a38b2a2c4358cd7beae
ArtemElectr <71963675+ArtemElectr@users.noreply.github.com>
2024-11-13 13:30:17 +0300

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session  # Сессия БД
 from backend.db_depends import get_db  # Функция подключения к базе данных
 from typing import Annotated  # Аннотации
-from models import User  # Модели БД
+from models import User, Task  # Модели БД
 from schemas import CreateUser, UpdateUser  # Pydantic.
 from sqlalchemy import insert, select, update, delete  # Функции работы с записями
 from slugify import slugify  # Функция работы с записями
@@ -14,7 +14,6 @@ router = APIRouter(prefix='/user', tags=['user'])
 @router.get('/')
 async def all_users(db: Annotated[Session, Depends(get_db)]):
     s = db.scalars(select(User)).all()
-    print(s)
     return s
 
 
@@ -28,6 +27,18 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
         )
     else:
         return user
+
+
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    task_this_user = db.scalars(select(Task).where(Task.user_id == user_id)).fetchall()
+    if not task_this_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Tasks this user are not found"
+        )
+    else:
+        return task_this_user
 
 
 @router.post('/create')
@@ -84,8 +95,10 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
         )
     else:
         db.execute(delete(User).where(User.id == user_id))
+        db.execute(delete(Task).where(Task.user_id == user_id))
         db.commit()
         return {
             "status_code": status.HTTP_200_OK,
             'transaction': 'User delete is successful!'
         }
+
